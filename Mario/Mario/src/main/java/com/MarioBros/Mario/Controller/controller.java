07a1096cd0bf6c68cb28bc1ebacd6887aa197b09
PN-Alumno <PN-Alumno@duoc.acad>
2026-03-17 16:40:44 -0300

    Spring MarioBros

---
+++ Mario/Mario/src/main/java/com/MarioBros/Mario/Controller/controller.java
@@ -0,0 +1,14 @@
+package com.MarioBros.Mario.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/luigi")
+    public String MarioBros() {
+        return "Mario y Luigi son distintas versiones de Phineas y Ferb :)";
+    }
+    
+}
